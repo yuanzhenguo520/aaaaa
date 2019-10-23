0ed3809461f1d38d5590fe9f4bdc11f592ad7a7f
yuanzhenguo <yuan.zhenguo@trans-cosmos.com.cn>
2019-10-23 09:57:21 +0800

    1

--- iii/src/iii/Yuanzhenguo.java
+++ iii/src/iii/Yuanzhenguo.java
@@ -3,6 +3,9 @@ package iii;
 public class Yuanzhenguo {
 
 	public static void main(String[] args) {
+		char[]helloArray={'y','u','a','n','z','h','e','n','g','u','o'};
+		String helloString=new String(helloArray);
+			System.out.println(helloString);
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
