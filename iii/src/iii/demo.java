693e37031b159aef9a440b975299cd14342223db
yuanzhenguo <yuan.zhenguo@trans-cosmos.com.cn>
2019-10-23 10:32:09 +0800

    uisgtug

--- iii/src/iii/demo.java
+++ iii/src/iii/demo.java
@@ -2,7 +2,7 @@ package iii;
 
 public class demo {
 	public static void main(String age[]){
-	char[]helloArray={'y','u','a','n','z','h','e','n','g','u','o'};
+	char[]helloArray={'y','u','a','n','z','h','e','n','g','u','o','s'};
 	String helloString=new String(helloArray);
 		System.out.println(helloString);
 
